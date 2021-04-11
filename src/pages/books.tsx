0e89e710d75cd9d850f882f9f4eccc92be3f5c96
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-11 18:10:02 +0900

    feat: booksページ作成

---
+++ src/pages/books.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Books } from '../components/Books';
+import { Layout } from '../components/Layout';
+
+const books: React.VFC = () => (
+  <Layout>
+    <Books />
+  </Layout>
+);
+
+export default books;
