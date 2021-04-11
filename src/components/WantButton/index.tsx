138247453029dd4d4fc9897a784ba911699eeaf5
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-11 18:05:21 +0900

    feat: WantButtonコンポーネントの作成

---
+++ src/components/WantButton/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+export const WantButton: React.VFC = () => (
+  <button type="button" className={styles.root}>
+    気になる
+  </button>
+);
