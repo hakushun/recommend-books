2f2b48dd3b5f61c24139d1b697e3727ba6f237eb
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-11 18:05:34 +0900

    feat: BookItemコンポーネントの作成

---
+++ src/components/BookItem/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react';
+import styles from './index.module.scss';
+import { ReadButton } from '../ReadButton';
+import { WantButton } from '../WantButton';
+
+export const BookItem: React.VFC = () => (
+  <li className={styles.item}>
+    <Link href="/">
+      <a className={styles.link}>
+        <div>
+          <h3 className={styles.title}>
+            達人プログラマー(第2版): 熟達に向けたあなたの旅
+          </h3>
+          <span className={styles.author}>Andrew Hunt and David Thomas</span>
+        </div>
+        <div className={styles.img}>
+          <Image src="/img/dummy.jpg" width="100" height="150" />
+        </div>
+        <div className={styles.info}>
+          <span>０人に読まれてます</span>
+          <span>０人が気になってます</span>
+        </div>
+      </a>
+    </Link>
+    <div className={styles.action}>
+      <ReadButton />
+      <WantButton />
+    </div>
+  </li>
+);
