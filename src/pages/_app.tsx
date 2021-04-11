7c0ffb751e3b9c56fd176ae8f7f2d7ecf5571473
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-11 18:03:20 +0900

    feat: _app.tsxの作成

---
+++ src/pages/_app.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useStore } from '../redux/configureStore';
+
+import '../styles/reset.scss';
+import '../styles/global.scss';
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps): React.ReactElement {
+  const store = useStore(pageProps.initialReduxState);
+  const router = useRouter();
+
+  const handleRouteChange = () => {
+    const main = document.getElementById('main');
+    main?.focus();
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+      <div id="overlay" />
+    </Provider>
+  );
+}
