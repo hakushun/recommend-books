6b989d682c92983a8b4dd9b7c830cff739d0573f
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-11 18:04:32 +0900

    feat: Footerコンポーネントの作成

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+export const Footer: React.VFC = () => (
+  <footer className={styles.footer}>
+    ©NIJIBOX CO.,LTD ALL RIGHTS RESERVED
+  </footer>
+);
