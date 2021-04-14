6974c02bb7cd4be5add53c2bc1accaebc739d9f6
Shun Nakano <nakano_syun@waku-2.com>
2021-04-14 18:57:57 +0900

    feat: searchBooks.tsの実装

---
+++ src/libs/api/searchBooks.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { SearchResult } from '../../redux/modules/searchResults';
+
+type SearchRowData = {
+  items: SearchResult[];
+  kind: string;
+  totalItems: number;
+};
+
+const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
+
+export const searchBooks = async (keyword: string): Promise<SearchResult[]> => {
+  if (keyword === '') return [];
+  const { data } = await axios.get<SearchRowData>(`${BASE_URL}?q=${keyword}`);
+  return data.items;
+};
