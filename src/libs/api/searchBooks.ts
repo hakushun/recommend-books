6cd813458718d464a46b6dcfd807d3a30bd7444c
Shun Nakano <nakano_syun@waku-2.com>
2021-04-14 20:55:44 +0900

    fix: ファイルパス修正

--- src/libs/api/searchBooks.ts
+++ src/libs/api/searchBooks.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SearchResult } from '../../redux/modules/searchResults';
+import { SearchResult } from '../../redux/modules/searchResult';
 
 type SearchRowData = {
   items: SearchResult[];
