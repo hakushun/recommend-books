cee7fd75545a7e2f216b28624c50202cd54469f6
Shun Nakano <nakano_syun@waku-2.com>
2021-04-14 20:55:12 +0900

    feat: useExternalSearch.tsにmodules/searchResult関連のロジック追加

--- src/hooks/useExternalSearch.ts
+++ src/hooks/useExternalSearch.ts
@@ -1,9 +1,23 @@
-import { useCallback, useRef } from 'react';
+import { MutableRefObject, useCallback, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+  SearchResult,
+  select,
+  selectSearchResult,
+} from '../redux/modules/searchResult';
 import { search, selectSearchResults } from '../redux/modules/searchResults';
 
-export const useExternalSearch = () => {
+type CustomHooks = () => {
+  inputRef: MutableRefObject<HTMLInputElement | null>;
+  searchResult: SearchResult;
+  searchResults: SearchResult[];
+  handleSubmit: (_e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+  handleSelect: (_result: SearchResult) => void;
+};
+
+export const useExternalSearch: CustomHooks = () => {
   const dispatch = useDispatch();
+  const searchResult = useSelector(selectSearchResult);
   const searchResults = useSelector(selectSearchResults);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
@@ -19,5 +33,9 @@ export const useExternalSearch = () => {
     [dispatch],
   );
 
-  return { inputRef, searchResults, handleSubmit };
+  const handleSelect = (result: SearchResult) => {
+    dispatch(select(result));
+  };
+
+  return { inputRef, searchResult, searchResults, handleSubmit, handleSelect };
 };
