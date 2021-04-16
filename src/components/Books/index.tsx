e14fba24ab6df9cbda20e00950627fa386591bce
Shun Nakano <nakano_syun@waku-2.com>
2021-04-16 15:40:46 +0900

    feat: useExternalSearchと接続

--- src/components/Books/index.tsx
+++ src/components/Books/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useExternalSearch } from '../../hooks/useExternalSearch';
 import { ExternalBookSearch } from '../ExternalBookSearch';
 import { SearchResult } from '../SearchResult';
 import styles from './index.module.scss';
 
-export const Books: React.VFC = () => (
-  <section className={styles.root}>
-    <h2 className={styles.title}>読んだ/読みたい本の登録</h2>
-    <ExternalBookSearch />
-    <SearchResult />
-  </section>
-);
+export const Books: React.VFC = () => {
+  const { inputRef, searchResults, handleSubmit } = useExternalSearch();
+
+  return (
+    <section className={styles.root}>
+      <h2 className={styles.title}>読んだ/読みたい本の登録</h2>
+      <ExternalBookSearch inputRef={inputRef} onSubmit={handleSubmit} />
+      <SearchResult results={searchResults} />
+    </section>
+  );
+};
