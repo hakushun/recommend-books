c3d6bce87b1c2c774c46f90c16d96023aeb4a97c
Shun Nakano <nakano_syun@waku-2.com>
2021-04-16 15:41:35 +0900

    feat: propsと接続

--- src/components/SearchResult/index.tsx
+++ src/components/SearchResult/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { SearchResultList } from '../SearchResultList';
+import { SearchResult as typeSearchResult } from '../../redux/modules/searchResult';
 import styles from './index.module.scss';
+import { BookRegisterDialog } from '../BookRegisterDialog';
 
-export const SearchResult: React.VFC = () => (
+type Props = {
+  results: typeSearchResult[];
+};
+export const SearchResult: React.VFC<Props> = ({ results }) => (
   <div className={styles.root}>
     <h3 className={styles.title}>検索結果</h3>
-    <SearchResultList />
+    <SearchResultList results={results} />
+    <BookRegisterDialog />
   </div>
 );
