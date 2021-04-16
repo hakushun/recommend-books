951380580e1e759dc5b63ce80fe4fc1bbb2708c9
Shun Nakano <nakano_syun@waku-2.com>
2021-04-16 15:45:13 +0900

    feat: useInternalSearchと接続

--- src/components/InternalBookSearch/index.tsx
+++ src/components/InternalBookSearch/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useInternalSearch } from '../../hooks/useInternalSearch';
 import styles from './index.module.scss';
 
-export const InternalBookSearch: React.VFC = () => (
-  <div className={styles.root}>
-    <div role="search" className={styles.wrapper}>
-      <input
-        type="text"
-        name="search_book"
-        id="search_book"
-        placeholder="みんなが読んだ/読みたい本を検索"
-        autoComplete="off"
-        className={styles.input}
-      />
+export const InternalBookSearch: React.VFC = () => {
+  const { searchword, handleSearch } = useInternalSearch();
+
+  return (
+    <div className={styles.root}>
+      <div role="search" className={styles.wrapper}>
+        <input
+          type="text"
+          name="search_book"
+          id="search_book"
+          placeholder="みんなが読んだ/読みたい本を検索"
+          autoComplete="off"
+          value={searchword}
+          onChange={handleSearch}
+          className={styles.input}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
