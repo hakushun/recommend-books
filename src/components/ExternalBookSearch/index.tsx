51feaa8b9c66b62688bb2fe077ff88c0e710e3dd
Shun Nakano <nakano_syun@waku-2.com>
2021-04-16 15:41:07 +0900

    feat: propsと接続

--- src/components/ExternalBookSearch/index.tsx
+++ src/components/ExternalBookSearch/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import styles from './index.module.scss';
 
-export const ExternalBookSearch: React.VFC = () => (
-  <form className={styles.root}>
+type Props = {
+  inputRef: React.MutableRefObject<HTMLInputElement | null>;
+  onSubmit: (_e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+};
+export const ExternalBookSearch: React.VFC<Props> = ({
+  inputRef,
+  onSubmit,
+}) => (
+  <form onSubmit={onSubmit} className={styles.root}>
     <div role="search" className={styles.wrapper}>
       <input
         type="text"
@@ -10,6 +17,7 @@ export const ExternalBookSearch: React.VFC = () => (
         id="search_book"
         placeholder="読んだ/読みたい本を検索"
         autoComplete="off"
+        ref={inputRef}
         className={styles.input}
       />
     </div>
