0a076f4cf6c8fd5f1bc9c0f49da68dbd461720f2
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-17 16:41:08 +0900

    refactor: useInternalSearch.tsに型定義

--- src/hooks/useInternalSearch.ts
+++ src/hooks/useInternalSearch.ts
@@ -2,7 +2,11 @@ import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { search, selectSearch } from '../redux/modules/search';
 
-export const useInternalSearch = () => {
+type CustomHooks = () => {
+  searchword: string;
+  handleSearch: (_e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+export const useInternalSearch: CustomHooks = () => {
   const dispatch = useDispatch();
   const searchword = useSelector(selectSearch);
 
