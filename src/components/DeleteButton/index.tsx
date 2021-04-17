e13310f6b44a570f0038169d3a907bbcad5360ec
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-17 18:21:36 +0900

    refactor: DeleteButtonにuserの接続

--- src/components/DeleteButton/index.tsx
+++ src/components/DeleteButton/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useBook } from '../../hooks/useBook';
+import { useUser } from '../../hooks/useUser';
 import { BookItem } from '../../redux/modules/book';
 import styles from './index.module.scss';
 
@@ -8,6 +9,11 @@ type Props = {
 };
 export const DeleteButton: React.VFC<Props> = ({ item }) => {
   const { isLoading, handleDelete } = useBook();
+  const { user } = useUser();
+
+  // 作成者以外には表示しない
+  if (user?.id !== item.registeredBy?.id) return null;
+
   return (
     <button
       type="button"
