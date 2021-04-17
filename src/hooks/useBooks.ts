05f8e5af7e4b042ea190bbf00a7cb74c2cdaadc5
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-17 16:40:17 +0900

    feat: subscribeメソッドの接続

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,19 +1,29 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { getInstance } from '../libs/firestore/getInstance';
 import { BookItem } from '../redux/modules/book';
-import { fetch, selectBooks, selectIsLoading } from '../redux/modules/books';
+import {
+  selectBooks,
+  selectIsLoading,
+  subscribe,
+} from '../redux/modules/books';
 
 type CustomHooks = () => {
   books: BookItem[];
   isLoading: boolean;
 };
 export const useBooks: CustomHooks = () => {
+  const db = getInstance();
   const dispatch = useDispatch();
   const books = useSelector(selectBooks);
   const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
-    dispatch(fetch());
+    db.collection('books').onSnapshot((snapshot) => {
+      const items: BookItem[] = [];
+      snapshot.forEach((doc) => items.push(doc.data() as BookItem));
+      dispatch(subscribe(items));
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
