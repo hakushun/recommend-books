a74a62682b6f69e1ac85a484306173a533c9df98
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-17 16:38:30 +0900

    feat: crudBook.tsにfirestoreメソッド実装

--- src/libs/firestore/crudBook.ts
+++ src/libs/firestore/crudBook.ts
@@ -4,6 +4,9 @@ import {
   ReactPayload,
 } from '../../redux/modules/book';
 import { getNow } from '../dayjs';
+import { getInstance } from './getInstance';
+
+const db = getInstance();
 
 const mapToBook = ({ item, user, type }: CreatePayload): BookItem => ({
   id: item.id,
@@ -20,13 +23,22 @@ const mapToBook = ({ item, user, type }: CreatePayload): BookItem => ({
   updatedAt: getNow(),
 });
 
+export const fetchBook = async (id: string): Promise<BookItem> => {
+  const doc = await db.collection('books').doc(id).get();
+  return doc.data() as BookItem;
+};
+
 export const createBook = async ({
   item,
   user,
   type,
 }: CreatePayload): Promise<void> => {
   const book = mapToBook({ item, user, type });
-  console.log('createBook', book);
+  // すでに登録済みであれば、DBへの登録はしないでreturn
+  const target = await db.collection('books').doc(book.id).get();
+  if (target.exists) throw new Error('すでに登録済みです');
+
+  await db.collection('books').doc(book.id).set(book);
 };
 
 export const reactBook = async ({
@@ -42,5 +54,9 @@ export const reactBook = async ({
     usersWantRead:
       type === 'want' ? [...item.usersWantRead, user] : [...item.usersWantRead],
   };
-  console.log('reactBook', book);
+  await db.collection('books').doc(book.id).set(book, { merge: true });
+};
+
+export const removeBook = async (item: BookItem): Promise<void> => {
+  await db.collection('books').doc(item.id).delete();
 };
