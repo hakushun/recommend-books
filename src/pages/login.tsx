becd06532c07daf40c1fc3ab9dc2f252cbc3c011
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-17 18:22:28 +0900

    feat: loginページ作成

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Login } from '../components/Login';
+import { Layout } from '../components/Layout';
+
+const login: React.VFC = () => (
+  <Layout>
+    <Login />
+  </Layout>
+);
+
+export default login;
