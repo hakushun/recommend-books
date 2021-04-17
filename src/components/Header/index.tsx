7a5a20d8db2a0f2426a2e39354f7c2157c3d6e51
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-17 18:21:18 +0900

    refactor: Headerにuserの接続

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,39 @@
 import Link from 'next/Link';
 import React from 'react';
+import { useUser } from '../../hooks/useUser';
 import styles from './index.module.scss';
 
-export const Header: React.VFC = () => (
-  <header className={styles.header}>
-    <div className={styles.inner}>
-      <h1 className={styles.title}>
-        <Link href="/">
-          <a className={styles.titleLink}>レコ読</a>
-        </Link>
-      </h1>
-      <nav>
-        <ul className={styles.navList}>
-          <li className={styles.navItem}>
-            <Link href="/books">
-              <a className={styles.navLink}>レコ読の追加</a>
-            </Link>
-          </li>
-          <li className={styles.navItem}>
-            <button type="button" className={styles.navLink}>
-              Logout
-            </button>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-);
+export const Header: React.VFC = () => {
+  const { user, logout } = useUser();
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <h1 className={styles.title}>
+          <Link href="/">
+            <a className={styles.titleLink}>レコ読</a>
+          </Link>
+        </h1>
+        {user && (
+          <nav>
+            <ul className={styles.navList}>
+              <li className={styles.navItem}>
+                <Link href="/books">
+                  <a className={styles.navLink}>レコ読の追加</a>
+                </Link>
+              </li>
+              <li className={styles.navItem}>
+                <button
+                  type="button"
+                  onClick={logout}
+                  className={styles.navLink}>
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </div>
+    </header>
+  );
+};
