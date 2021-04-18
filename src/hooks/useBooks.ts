cbad74cc12a913568bf6082bb8f5288f6a9d79ec
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-18 16:32:58 +0900

    feat: useBooks.tsにhaveRegistered関数実装

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -7,16 +7,21 @@ import {
   selectIsLoading,
   subscribe,
 } from '../redux/modules/books';
+import { selectSearchResult } from '../redux/modules/searchResult';
 
 type CustomHooks = () => {
   books: BookItem[];
   isLoading: boolean;
+  haveRegistered: boolean;
 };
 export const useBooks: CustomHooks = () => {
   const db = getInstance();
   const dispatch = useDispatch();
   const books = useSelector(selectBooks);
   const isLoading = useSelector(selectIsLoading);
+  const searchResult = useSelector(selectSearchResult);
+
+  const haveRegistered = books.some((book) => book.id === searchResult.id);
 
   useEffect(() => {
     db.collection('books').onSnapshot((snapshot) => {
@@ -27,5 +32,5 @@ export const useBooks: CustomHooks = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return { books, isLoading };
+  return { books, isLoading, haveRegistered };
 };
