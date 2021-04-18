86540279566870f4c270f826cd7d8e0ffb5bd977
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-18 16:38:12 +0900

    feat: BookDetailコンポーネントの作成

---
+++ src/components/BookDetail/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { BookItem } from '../../redux/modules/book';
+import { DeleteButton } from '../DeleteButton';
+import { PreviewLink } from '../PreviewLink';
+import { ReactionButton } from '../ReactionButton';
+import styles from './index.module.scss';
+
+type Props = {
+  book: BookItem;
+};
+export const BookDetail: React.VFC<Props> = ({ book }) => (
+  <section className={styles.root}>
+    <div className={styles.detail}>
+      <div>
+        <div className={styles.imgWrapper}>
+          <img
+            src={book.imageUrl || '/img/no-images.png'}
+            alt="book cover"
+            width="150"
+            className={styles.img}
+          />
+        </div>
+        <div className={styles.action}>
+          <PreviewLink link={book.previewLink} />
+          <ReactionButton type="read" item={book} />
+          <ReactionButton type="want" item={book} />
+          <DeleteButton item={book} />
+        </div>
+      </div>
+      <dl className={styles.list}>
+        <div className={styles.listItem}>
+          <dt>Title</dt>
+          <dd>{book.title}</dd>
+        </div>
+        <div className={styles.listItem}>
+          <dt>Author</dt>
+          <dd>{book.authors.join(', ')}</dd>
+        </div>
+        <div className={styles.listItem}>
+          <dt>Description</dt>
+          <dd>{book.description}</dd>
+        </div>
+        <div className={styles.listItem}>
+          <dt>Registered By</dt>
+          <dd>{book.registeredBy?.name}</dd>
+        </div>
+        <div className={styles.listItem}>
+          <dt>読んだ人</dt>
+          <dd>{book.usersHaveRead.map((user) => user?.name).join(', ')}</dd>
+        </div>
+        <div className={styles.listItem}>
+          <dt>読みたい人</dt>
+          <dd>{book.usersWantRead.map((user) => user?.name).join(', ')}</dd>
+        </div>
+      </dl>
+    </div>
+  </section>
+);
