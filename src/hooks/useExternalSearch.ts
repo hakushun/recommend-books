4b92ccfa216b4ac746458630b95d7d0f8e95a055
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-18 16:33:47 +0900

    feat: useExternalSearch.tsにpageCountとhandlePagenationの実装

--- src/hooks/useExternalSearch.ts
+++ src/hooks/useExternalSearch.ts
@@ -1,41 +1,80 @@
-import { MutableRefObject, useCallback, useRef } from 'react';
+import React, { MutableRefObject, useCallback, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   SearchResult,
   select,
   selectSearchResult,
 } from '../redux/modules/searchResult';
-import { search, selectSearchResults } from '../redux/modules/searchResults';
+import {
+  search,
+  selectMaxResults,
+  selectSearchResults,
+  selectTotalItems,
+} from '../redux/modules/searchResults';
 
 type CustomHooks = () => {
   inputRef: MutableRefObject<HTMLInputElement | null>;
   searchResult: SearchResult;
   searchResults: SearchResult[];
+  pageCount: number;
   handleSubmit: (_e: React.FormEvent<HTMLFormElement>) => Promise<void>;
   handleSelect: (_result: SearchResult) => void;
+  handlePagenation: (_selected: { selected: number }) => void;
 };
 
 export const useExternalSearch: CustomHooks = () => {
   const dispatch = useDispatch();
   const searchResult = useSelector(selectSearchResult);
   const searchResults = useSelector(selectSearchResults);
+  const totalItems = useSelector(selectTotalItems);
+  const maxResults = useSelector(selectMaxResults);
   const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const pageCount = Math.ceil(totalItems / maxResults);
 
   const handleSubmit = useCallback(
     async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      if (!inputRef.current?.value) {
+      if (!inputRef.current || inputRef.current.value.trim() === '') {
         inputRef.current?.focus();
         return;
       }
-      dispatch(search(inputRef.current.value));
+      dispatch(
+        search({
+          keyword: inputRef.current.value,
+          maxResults,
+          startIndex: 0,
+        }),
+      );
     },
-    [dispatch],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
   );
+  const handlePagenation = ({ selected }: { selected: number }) => {
+    if (!inputRef.current || inputRef.current.value.trim() === '') {
+      inputRef.current?.focus();
+      return;
+    }
+    dispatch(
+      search({
+        keyword: inputRef.current.value,
+        maxResults,
+        startIndex: selected * maxResults,
+      }),
+    );
+  };
 
   const handleSelect = (result: SearchResult) => {
     dispatch(select(result));
   };
 
-  return { inputRef, searchResult, searchResults, handleSubmit, handleSelect };
+  return {
+    inputRef,
+    searchResult,
+    searchResults,
+    pageCount,
+    handleSubmit,
+    handleSelect,
+    handlePagenation,
+  };
 };
