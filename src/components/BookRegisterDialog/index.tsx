3d6d1d38db16e2a7c3f99dbe51104a46bcf56677
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-18 16:40:10 +0900

    feat: BookRegisterDialogに登録済みの記述追加(WIP)

--- src/components/BookRegisterDialog/index.tsx
+++ src/components/BookRegisterDialog/index.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/Link';
 import React from 'react';
+import { useBooks } from '../../hooks/useBooks';
 import { useExternalSearch } from '../../hooks/useExternalSearch';
 import { useModal } from '../../hooks/useModal';
 import { formatAuthors } from '../../libs/utils/formatAuthors';
@@ -9,6 +11,7 @@ import styles from './index.module.scss';
 export const BookRegisterDialog: React.VFC = () => {
   const { registerDialog } = useModal();
   const { searchResult } = useExternalSearch();
+  const { haveRegistered } = useBooks();
 
   return (
     <>
@@ -22,10 +25,19 @@ export const BookRegisterDialog: React.VFC = () => {
               {formatAuthors(searchResult.volumeInfo.authors)}
             </span>
           </div>
-          <div className={styles.action}>
-            <RegisterButton type="read" item={searchResult} />
-            <RegisterButton type="want" item={searchResult} />
-          </div>
+          {haveRegistered ? (
+            <div>
+              <div>すでに登録済みです。</div>
+              <Link href={`/book/${searchResult.id}`}>
+                <a>{searchResult.volumeInfo.title}のページへ</a>
+              </Link>
+            </div>
+          ) : (
+            <div className={styles.action}>
+              <RegisterButton type="read" item={searchResult} />
+              <RegisterButton type="want" item={searchResult} />
+            </div>
+          )}
         </Modal>
       )}
     </>
