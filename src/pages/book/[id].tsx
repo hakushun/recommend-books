14cd9daed7235fca489b332b930cc0ad5f9d7803
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-18 16:38:43 +0900

    feat: bookページの作成(WIP)

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { Layout } from '../../components/Layout';
+import { useBook } from '../../hooks/useBook';
+import { BookDetail } from '../../components/BookDetail';
 
 const Book: React.VFC = () => {
   const router = useRouter();
   const { id } = router.query;
+  // ここのエラーの消し方わからない
+  const { book, isLoading } = useBook(id);
 
-  return <Layout>Book Id: {id}</Layout>;
+  if (!book) {
+    router.push('/');
+    return null;
+  }
+
+  return (
+    <Layout>
+      {isLoading ? <div>loading </div> : <BookDetail book={book} />}
+    </Layout>
+  );
 };
 
 export default Book;
