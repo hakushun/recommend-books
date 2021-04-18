627b4fdcd7c19751b77f666d388572e8ca1b040d
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-18 16:29:02 +0900

    refactor: searchBooks関数にmaxResultsとstartIndexを追加

--- src/libs/api/searchBooks.ts
+++ src/libs/api/searchBooks.ts
@@ -1,16 +1,18 @@
 import axios from 'axios';
-import { SearchResult } from '../../redux/modules/searchResult';
-
-type SearchRowData = {
-  items: SearchResult[];
-  kind: string;
-  totalItems: number;
-};
+import {
+  SearchPayload,
+  SearchRowData,
+} from '../../redux/modules/searchResults';
 
 const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
 
-export const searchBooks = async (keyword: string): Promise<SearchResult[]> => {
-  if (keyword === '') return [];
-  const { data } = await axios.get<SearchRowData>(`${BASE_URL}?q=${keyword}`);
-  return data.items || [];
+export const searchBooks = async ({
+  keyword,
+  maxResults,
+  startIndex,
+}: SearchPayload): Promise<SearchRowData> => {
+  const { data } = await axios.get<SearchRowData>(
+    `${BASE_URL}?maxResults=${maxResults}&startIndex=${startIndex}&q=${keyword}`,
+  );
+  return data;
 };
