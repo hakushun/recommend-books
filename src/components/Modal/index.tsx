ed1c8c1cdae872fb5445081db01162e646e5a0fb
Shun Nakano <nakano_syun@waku-2.com>
2021-04-19 12:20:53 +0900

    refactor: アクセシビリティ処理

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CloseButton } from '../CloseButton';
 import { Overlay } from '../Overlay';
 import styles from './index.module.scss';
@@ -6,11 +6,22 @@ import styles from './index.module.scss';
 type Props = {
   context: string;
 };
-export const Modal: React.FC<Props> = ({ context, children }) => (
-  <Overlay>
-    <div role="dialog" aria-modal="true" className={styles.root}>
-      {children}
-      <CloseButton context={context} />
-    </div>
-  </Overlay>
-);
+export const Modal: React.FC<Props> = ({ context, children }) => {
+  useEffect(() => {
+    document.getElementById('app')?.setAttribute('aria-hidden', 'true');
+
+    return () => {
+      document.getElementById('app')?.removeAttribute('aria-hidden');
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Overlay>
+      <div role="dialog" aria-modal="true" className={styles.root}>
+        {children}
+        <CloseButton context={context} />
+      </div>
+    </Overlay>
+  );
+};
