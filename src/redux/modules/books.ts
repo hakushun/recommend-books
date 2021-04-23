b1b2c45f0733e189116a89dd35a63e50d8acf22f
Shun Nakano <nakano_syun@waku-2.com>
2021-04-23 11:58:28 +0900

    feat: booksにsortの接続

--- src/redux/modules/books.ts
+++ src/redux/modules/books.ts
@@ -3,6 +3,7 @@ import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { BookItem } from './book';
 import { RootState } from './reducers';
+import { SortKey } from './sort';
 
 // types
 export type Books = {
@@ -36,21 +37,42 @@ const reducer = reducerWithInitialState(INITIAL_STATE).case(
 );
 export default reducer;
 
+// function
+const searchBooks = (searchword: string, books: BookItem[]): BookItem[] =>
+  books.filter((book) => {
+    const re = new RegExp(searchword, 'i');
+    return (
+      re.test(book.title) ||
+      re.test(book.authors.join()) ||
+      re.test(book.description)
+    );
+  });
+
+const sortBooks = (key: SortKey, books: BookItem[]): BookItem[] => {
+  switch (key) {
+    case 'new':
+      return books.sort((a, b) => b.createdAt - a.createdAt);
+    case 'read':
+      return books.sort(
+        (a, b) => b.usersHaveRead.length - a.usersHaveRead.length,
+      );
+    case 'want':
+      return books.sort(
+        (a, b) => b.usersWantRead.length - a.usersWantRead.length,
+      );
+    default:
+      return books;
+  }
+};
+
 // selectors
 export const selectBooks = createSelector(
   [
     (state: RootState) => state.resources.books.books,
     (state: RootState) => state.ui.search.searchword,
+    (state: RootState) => state.ui.sort.key,
   ],
-  (books, searchword) =>
-    books
-      .filter(
-        (book) =>
-          book.title.includes(searchword) ||
-          book.authors.join().includes(searchword) ||
-          book.description.includes(searchword),
-      )
-      .sort((a, b) => b.createdAt - a.createdAt),
+  (books, searchword, key) => sortBooks(key, searchBooks(searchword, books)),
 );
 
 export const selectIsLoading = createSelector(
