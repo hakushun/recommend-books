7cf8b77aae7a6daf2eac496813b39b3b37adf65e
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-24 17:07:08 +0900

    feat: modules/comment.tsの実装

--- src/redux/modules/comment.ts
+++ src/redux/modules/comment.ts
@@ -1,15 +1,105 @@
+import { createSelector } from 'reselect';
+import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { asyncFactory } from 'typescript-fsa-redux-thunk';
+import { createComment, removeComment } from '../../libs/firestore/crudComment';
+import { RootState } from './reducers';
 import { Userdata } from './user';
 
-export type Comment = {
+export type CommentItem = {
   id: string;
   content: string;
   author: Userdata;
   createdAt: number;
   updatedAt: number;
 };
+export type CreatePayload = {
+  bookId: string;
+  content: string;
+  author: Userdata;
+};
+export type RemovePayload = {
+  id: string;
+  bookId: string;
+};
+interface CustomError extends Error {
+  name: string;
+  message: string;
+}
+export type Comment = {
+  item: CommentItem;
+  isLoading: boolean;
+  error?: CustomError;
+};
+// actions
+const actionCreator = actionCreatorFactory();
+const asyncActionCreator = asyncFactory<CommentItem>(actionCreator);
 
-const INITIAL_STATE = {};
+export const create = asyncActionCreator<CreatePayload, void, CustomError>(
+  'CREATE_COMMENT',
+  async (params) => {
+    await createComment(params);
+  },
+);
+export const edit = actionCreator<CommentItem>('EDIT_COMMENT');
+export const remove = asyncActionCreator<RemovePayload, void, CustomError>(
+  'REMOVE_COMMENT',
+  async (params) => {
+    await removeComment(params);
+  },
+);
+const INITIAL_STATE: Comment = {
+  item: {
+    id: '',
+    content: '',
+    author: null,
+    createdAt: 0,
+    updatedAt: 0,
+  },
+  isLoading: false,
+};
 
-const reducer = reducerWithInitialState(INITIAL_STATE);
+// reducer
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(create.async.started, (state) => ({
+    ...state,
+    isLoading: true,
+  }))
+  .case(create.async.failed, (state, { error }) => ({
+    ...state,
+    isLoading: false,
+    error,
+  }))
+  .case(create.async.done, (state) => ({
+    ...state,
+    isLoading: false,
+  }))
+  .case(edit, (state, payload) => ({
+    ...state,
+    ...payload,
+  }))
+  .case(remove.async.started, (state) => ({
+    ...state,
+    isLoading: true,
+  }))
+  .case(remove.async.failed, (state, { error }) => ({
+    ...state,
+    isLoading: false,
+    error,
+  }))
+  .case(remove.async.done, (state) => ({
+    ...state,
+    isLoading: false,
+  }));
 export default reducer;
+
+// selectors
+export const selectComment = createSelector(
+  [(state: RootState) => state.resources.comment.item],
+  (comment) => comment,
+);
+
+export const selectIsLoading = createSelector(
+  [(state: RootState) => state.resources.comment.isLoading],
+  (isLoading) => isLoading,
+);
