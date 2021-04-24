d03e1d8b1daa4f74079039effb06d9c49abe2d34
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-24 18:37:24 +0900

    feat: UpdateCommentButtonコンポーネントの作成

---
+++ src/components/CommentButton/UpdateCommentButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { CommentItem, UpdatePayload } from '../../redux/modules/comment';
+import styles from './index.module.scss';
+
+type Props = {
+  bookId: string;
+  item: CommentItem;
+  isLoading: boolean;
+  handleUpdate: (_: UpdatePayload) => void;
+};
+export const UpdateCommentButton: React.VFC<Props> = ({
+  bookId,
+  item,
+  isLoading,
+  handleUpdate,
+}) => (
+  <button
+    type="button"
+    disabled={isLoading}
+    onClick={() => handleUpdate({ bookId, item })}
+    className={styles.create}>
+    更新
+  </button>
+);
