c8104db0b0f73c61af9fbf28f6aa89eef778bafd
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-24 21:27:40 +0900

    refactor:

--- src/components/GoogleButton/index.tsx
+++ src/components/GoogleButton/index.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { useUser } from '../../hooks/useUser';
 import styles from './index.module.scss';
 
-export const GoogleButton: React.FC = () => {
-  const { signinWithGoogle } = useUser();
-
-  return (
-    <button
-      type="button"
-      className={styles.google}
-      aria-label="Sign In with Google account"
-      onClick={signinWithGoogle}>
-      <img
-        className={styles.googleImg}
-        alt="Google icon"
-        src="/icon/google.svg"
-        width="18"
-        height="18"
-      />
-      <span className={styles.googleText}>Sign in with Google</span>
-    </button>
-  );
+type Props = {
+  signinWithGoogle: () => Promise<void>;
 };
+export const GoogleButton: React.VFC<Props> = ({ signinWithGoogle }) => (
+  <button
+    type="button"
+    className={styles.google}
+    aria-label="Sign In with Google account"
+    onClick={signinWithGoogle}>
+    <img
+      className={styles.googleImg}
+      alt="Google icon"
+      src="/icon/google.svg"
+      width="18"
+      height="18"
+    />
+    <span className={styles.googleText}>Sign in with Google</span>
+  </button>
+);
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useUser } from '../../hooks/useUser';
 import { GoogleButton } from '../GoogleButton';
 import styles from './index.module.scss';
 
-export const Login: React.VFC = () => (
-  <section className={styles.root}>
-    <GoogleButton />
-  </section>
-);
+export const Login: React.VFC = () => {
+  const { signinWithGoogle } = useUser();
+
+  return (
+    <section className={styles.root}>
+      <GoogleButton signinWithGoogle={signinWithGoogle} />
+    </section>
+  );
+};
