de3777471f0f1951623f8a20a942f44c760e7da9
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-24 17:09:33 +0900

    feat: CommentItemコンポーネントの作成

---
+++ src/components/CommentItem/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { CommentItem as typeComment } from '../../redux/modules/comment';
+import { DeleteCommentButton } from '../CommentButton/DeleteCommentButton';
+import { EditCommentButton } from '../CommentButton/EditCommentButton';
+import styles from './index.module.scss';
+
+type Props = {
+  bookId: string;
+  comment: typeComment;
+};
+export const CommentItem: React.VFC<Props> = ({ bookId, comment }) => (
+  <li className={styles.item}>
+    <div className={styles.header}>
+      <div>
+        <div className={styles.name}>{comment.author?.name}</div>
+        <div className={styles.date}>{comment.createdAt}</div>
+      </div>
+      <div className={styles.action}>
+        <EditCommentButton comment={comment} />
+        <DeleteCommentButton id={comment.id} bookId={bookId} />
+      </div>
+    </div>
+    <div className={styles.comment}>{comment.content}</div>
+  </li>
+);
