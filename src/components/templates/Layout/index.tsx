1c2a1fd569cabe35c03528f26bdd3f0d2f33b38d
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-25 13:28:30 +0900

    chore: ディレクトリ変更

--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Footer } from '../Footer';
-import { Header } from '../Header';
+import { Footer } from '../../molecules/Footer';
+import { Header } from '../../molecules/Header';
 import styles from './index.module.scss';
 
 export const Layout: React.FC = ({ children }) => (
