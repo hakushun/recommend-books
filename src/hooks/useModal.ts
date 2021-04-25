4c0afc32f71ca5b10eb139f20d01dcaa421dcb72
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-25 10:11:19 +0900

    refactor: useModal.tsにロジック集約

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,21 +1,49 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  selectRegisterDialog,
-  toggle,
-  TogglePayload,
-} from '../redux/modules/modal';
+import { selectModal, toggle, TogglePayload } from '../redux/modules/modal';
+import { useExternalSearch } from './useExternalSearch';
 
-type Hooks = () => {
-  registerDialog: boolean;
+type Hooks = (
+  _context: string,
+) => {
+  isOpened: boolean;
   handleToggle: (_payload: TogglePayload) => void;
 };
 
-export const useModal: Hooks = () => {
+export const useModal: Hooks = (context: string) => {
+  const router = useRouter();
   const dispatch = useDispatch();
-  const registerDialog = useSelector(selectRegisterDialog);
+  const modal = useSelector(selectModal);
+  const { handleReset } = useExternalSearch();
+
+  const isOpened = modal[context];
 
   const handleToggle = (payload: TogglePayload) => {
     dispatch(toggle(payload));
   };
-  return { registerDialog, handleToggle };
+
+  useEffect(() => {
+    document.getElementById('app')?.removeAttribute('aria-hidden');
+    if (isOpened) {
+      document.getElementById('app')?.setAttribute('aria-hidden', 'true');
+    }
+  }, [isOpened]);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      handleToggle({ registerDialog: false });
+      handleReset();
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      document.getElementById('app')?.removeAttribute('aria-hidden');
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { isOpened, handleToggle };
 };
