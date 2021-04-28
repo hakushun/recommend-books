e0d39e2f76008aac32ef2d194440504212e23f56
Shun Nakano <nakano_syun@waku-2.com>
2021-04-28 12:33:40 +0900

    feat: getServerSideProps追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import { Home } from '../components/organisms/Home';
+import { getBooks } from '../libs/cloudFunctions/getBooks';
+import { BookItem } from '../redux/modules/book';
+import { subscribe } from '../redux/modules/books';
+import { wrapper } from '../redux/store';
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async ({ store }) => {
+    const books: BookItem[] = await getBooks();
+    store.dispatch(subscribe(books));
+  },
+);
 
 const index: React.VFC = () => <Home />;
 
