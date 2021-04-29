3a69cbc6aa8fc1a8e2d85339b8ad26d3f86d0056
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-29 14:09:51 +0900

    test: add another test case

--- src/libs/utils/__tests__/mapUserData.test.ts
+++ src/libs/utils/__tests__/mapUserData.test.ts
@@ -6,13 +6,25 @@ describe('mapUserData', () => {
     email: 'sample@sample.com',
     uid: '12345',
   };
+  const emptyUser: FirebaseUser = {
+    displayName: '',
+    email: '',
+    uid: '12345',
+  };
   it('正常値', () => {
     const result = {
       id: '12345',
       email: 'sample@sample.com',
       name: 'test user',
     };
-
     expect(result).toEqual(mapUserData(firebaseUser));
+  });
+  it('情報不足', () => {
+    const result = {
+      id: '12345',
+      email: '',
+      name: '',
+    };
+    expect(result).toEqual(mapUserData(emptyUser));
   });
 });
