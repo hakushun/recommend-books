5749f4cf5cdf1ea75c96df827672afef282a3506
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-30 18:13:25 +0900

    feat: TagsEditListコンポーネントの作成

---
+++ src/components/molecules/TagsEditList/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Tag } from '../../../redux/modules/tags';
+import { DeleteButon } from '../../atoms/TagButton/DeleteButon';
+import styles from './index.module.scss';
+
+type Props = {
+  tags: Tag[];
+  handleRemove: (_id: string) => void;
+};
+export const TagsEditList: React.VFC<Props> = ({ tags, handleRemove }) => (
+  <ul className={styles.list}>
+    {tags.map((tag) => (
+      <li key={tag.id} className={styles.item}>
+        {tag.value}
+        <DeleteButon tag={tag} handleRemove={handleRemove} />
+      </li>
+    ))}
+  </ul>
+);
