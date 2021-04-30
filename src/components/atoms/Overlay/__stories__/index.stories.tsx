babaea23a8fce83344ef29358a91783916f49ce8
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-30 09:57:02 +0900

    test: story追加

---
+++ src/components/atoms/Overlay/__stories__/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { Overlay } from '..';
+
+export default {
+  title: 'atoms/Overlay',
+  component: Overlay,
+} as Meta;
+
+const Template: Story = (args) => <Overlay {...args} />;
+// Error: Target container is not a DOM element.
+export const Default = Template.bind({});
