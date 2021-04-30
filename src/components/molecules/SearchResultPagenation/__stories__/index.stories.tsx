fd44d5cea9957a8d9c22864ac75c749baf394757
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-04-30 09:59:47 +0900

    test: story追加

---
+++ src/components/molecules/SearchResultPagenation/__stories__/index.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { SearchResultPagenation, Props } from '..';
+
+export default {
+  title: 'molecules/SearchResultPagenation',
+  component: SearchResultPagenation,
+} as Meta;
+
+const Template: Story<Props> = (args) => <SearchResultPagenation {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  pageCount: 70,
+  handlePagenation: action('handlePagenation'),
+};
