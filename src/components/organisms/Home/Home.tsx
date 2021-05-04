b6eefe483254471dd1d7a879958e1be90280af4a
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-05-04 13:55:18 +0900

    feat: TagFilterの接続

--- src/components/organisms/Home/Home.tsx
+++ src/components/organisms/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BookList } from '../../molecules/BookList';
 import { InternalBookSearch } from '../../molecules/InternalBookSearch';
 import { SortSelectBox } from '../../molecules/SortSelectBox';
+import { TagFilter } from '../../molecules/TagFilter';
 import styles from './index.module.scss';
 
 export const Home: React.VFC = () => (
   <section className={styles.root}>
     <h2 className={styles.title}>みんなが読んだ/読みたい本</h2>
     <InternalBookSearch />
+    <TagFilter />
     <SortSelectBox />
     <BookList />
   </section>
