24e36d5b7844a3fbc59392b1b1cbab39d80830a9
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-05-06 18:54:13 +0900

    refactor: redux devtoolを本番にも適用

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,12 +4,8 @@ import { createWrapper } from 'next-redux-wrapper';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './modules/reducers';
 
-const bindMiddleware = (middleware: Middleware[]) => {
-  if (process.env.NODE_ENV !== 'production') {
-    return composeWithDevTools(applyMiddleware(...middleware));
-  }
-  return applyMiddleware(...middleware);
-};
+const bindMiddleware = (middleware: Middleware[]) =>
+  composeWithDevTools(applyMiddleware(...middleware));
 
 export const initStore = (): Store =>
   createStore(rootReducer, bindMiddleware([thunkMiddleware]));
