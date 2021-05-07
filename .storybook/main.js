f72bbc72d54c21db04a884317bdabb6316802dfe
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-05-07 10:07:33 +0900

    chore: config修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,24 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/preset-scss',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'resolve-url-loader',
+          options: {
+            root: path.resolve(__dirname, '../public'),
+          },
+        },
+        'sass-loader',
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
 };
