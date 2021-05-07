b8406a5db32315ac3486f6a32d13023d2aee4f9d
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-05-07 14:54:25 +0900

    fix: typo

--- src/components/atoms/TagDeleteButton/__stories__/index.stories.tsx
+++ src/components/atoms/TagDeleteButton/__stories__/index.stories.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { TagDeleteButon, Props } from '..';
+import { TagDeleteButton, Props } from '..';
 
 export default {
-  title: 'atoms/TagDeleteButon',
-  component: TagDeleteButon,
+  title: 'atoms/TagDeleteButton',
+  component: TagDeleteButton,
 } as Meta;
 
-const Template: Story<Props> = (args) => <TagDeleteButon {...args} />;
+const Template: Story<Props> = (args) => <TagDeleteButton {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
