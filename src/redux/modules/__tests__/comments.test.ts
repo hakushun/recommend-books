19b03d9ba3a11cc983a49e4370900486f2c1cd33
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-05-08 11:56:13 +0900

    test: selectorのtest作成

--- src/redux/modules/__tests__/comments.test.ts
+++ src/redux/modules/__tests__/comments.test.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-undefined */
-import reducer, { subscribe } from '../comments';
+import reducer, { selectComments, subscribe } from '../comments';
+import { initialState } from './_initialState';
 
 describe('Reducer: comments', () => {
   const user = {
@@ -38,6 +39,86 @@ describe('Reducer: comments', () => {
   });
 });
 
-// describe('Selector: comments', () => {
-//   it('selectComments', () => {});
-// });
+describe('Selector: comments', () => {
+  const state = {
+    ...initialState,
+    resources: {
+      ...initialState.resources,
+      comments: {
+        comments: [
+          {
+            id: '00001',
+            content: 'サンプルコメント1',
+            author: {
+              name: 'sample user1',
+              id: '315X13qvz7N847FJPYpDg6KLUMz1',
+              email: 'shun.nakano@nijibox.co.jp',
+            },
+            createdAt: 1620438954214 + 100,
+            updatedAt: 1620438954214 + 100,
+          },
+          {
+            id: '00002',
+            content: 'サンプルコメント2',
+            author: {
+              name: 'sample user2',
+              id: '315X13qvz7N847FJPYpDg6KLUMz1',
+              email: 'shun.nakano@nijibox.co.jp',
+            },
+            createdAt: 1620438954214,
+            updatedAt: 1620438954214,
+          },
+          {
+            id: '00003',
+            content: 'サンプルコメント3',
+            author: {
+              name: 'sample user3',
+              id: '315X13qvz7N847FJPYpDg6KLUMz1',
+              email: 'shun.nakano@nijibox.co.jp',
+            },
+            createdAt: 1620438954214 + 300,
+            updatedAt: 1620438954214 + 300,
+          },
+        ],
+      },
+    },
+  };
+  it('selectComments', () => {
+    const result = [
+      {
+        id: '00002',
+        content: 'サンプルコメント2',
+        author: {
+          name: 'sample user2',
+          id: '315X13qvz7N847FJPYpDg6KLUMz1',
+          email: 'shun.nakano@nijibox.co.jp',
+        },
+        createdAt: 1620438954214,
+        updatedAt: 1620438954214,
+      },
+      {
+        id: '00001',
+        content: 'サンプルコメント1',
+        author: {
+          name: 'sample user1',
+          id: '315X13qvz7N847FJPYpDg6KLUMz1',
+          email: 'shun.nakano@nijibox.co.jp',
+        },
+        createdAt: 1620438954214 + 100,
+        updatedAt: 1620438954214 + 100,
+      },
+      {
+        id: '00003',
+        content: 'サンプルコメント3',
+        author: {
+          name: 'sample user3',
+          id: '315X13qvz7N847FJPYpDg6KLUMz1',
+          email: 'shun.nakano@nijibox.co.jp',
+        },
+        createdAt: 1620438954214 + 300,
+        updatedAt: 1620438954214 + 300,
+      },
+    ];
+    expect(result).toEqual(selectComments(state));
+  });
+});
