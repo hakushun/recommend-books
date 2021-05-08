01f23e458d7f4e4d43c35d5dbc327e4836ae9adb
Shun Nakano <shun.nakano@nijibox.co.jp>
2021-05-08 11:54:43 +0900

    test: テスト用のinitial state作成

---
+++ src/redux/modules/__tests__/_initialState.ts
@@ -0,0 +1,79 @@
+import { RootState } from '../reducers';
+
+export const initialState: RootState = {
+  resources: {
+    user: {
+      user: null,
+      isLoading: false,
+    },
+    book: {
+      item: {
+        id: '',
+        title: '',
+        authors: [],
+        description: '',
+        previewLink: '',
+        imageUrl: '',
+        usersHaveRead: [],
+        usersWantRead: [],
+        registeredBy: null,
+        tags: [],
+        createdAt: 0,
+        updatedAt: 0,
+      },
+      isLoading: false,
+    },
+    books: {
+      books: [],
+      page: 0,
+      maxResults: 30,
+    },
+    comment: {
+      item: {
+        id: '',
+        content: '',
+        author: null,
+        createdAt: 0,
+        updatedAt: 0,
+      },
+      isLoading: false,
+    },
+    comments: {
+      comments: [],
+    },
+    searchResults: {
+      searchResults: [],
+      totalItems: 0,
+      maxResults: 20,
+      isLoading: false,
+    },
+  },
+  ui: {
+    searchResult: {
+      id: '',
+      volumeInfo: {
+        authors: [],
+        description: '',
+        imageLinks: {
+          thumbnail: '',
+        },
+        title: '',
+        previewLink: '',
+      },
+    },
+    modal: {
+      registerDialog: false,
+    },
+    search: {
+      searchword: '',
+    },
+    sort: {
+      key: 'new',
+    },
+    tags: {
+      tags: [],
+      isEditable: false,
+      selected: null,
+    },
+  },
+};
